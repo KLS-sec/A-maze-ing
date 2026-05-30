648d3ca62b79e22c323ff368a54f9f7203f219b1
Mahammad Makhmaev <mmakhmae@c1r2p5.42nice.fr>
2026-05-30 19:27:31 +0200

    RENDU maj

--- RENDU/a_maze_ing.py
+++ RENDU/a_maze_ing.py
@@ -1,7 +1,11 @@
 from colorama import Fore, Style, init
 import os
 import sys
+from pydantic import BaseModel, Field, model_validator, ValidationError
+import re
 from maze_generator import maze_generator
+from maze_generator.maze_generator import MazeGenerator
+from config_parsing import Config
 
 
 init(autoreset=True)  # reset de couleurs
@@ -79,6 +83,130 @@ def color_map(maze: list[list[maze_generator.Cell]], height: int, width: int,
     print(colors["wall"] + "oooo" * width + "o" + Style.RESET_ALL)
 
 
+class config_storage(BaseModel):
+    """Multiple check for the config file input."""
+    width: int = Field(ge=2, le=50)
+    height: int = Field(ge=2, le=20)
+    entry: list[int]
+    exit: list[int]
+    output_file: str
+    perfect: bool
+
+    @model_validator(mode='after')
+    def checker(self) -> "config_storage":
+        if len(self.entry) != 2:
+            raise ValueError("Entry error, invalid coordinates")
+        if len(self.exit) != 2:
+            raise ValueError("Exit error, invalid coordinates")
+        if (
+           self.entry[0] < 0 or self.entry[0] >= self.width or
+           self.entry[1] < 0 or self.entry[1] >= self.height):
+            raise ValueError("Entry error, invalid coordinates")
+        if (
+           self.exit[0] < 0 or self.exit[0] >= self.width or
+           self.exit[1] < 0 or self.exit[1] >= self.height):
+            raise ValueError("Exit error, invalid coordinates")
+
+        if not re.fullmatch(r"[A-Za-z0-9_]+\.txt", self.output_file):
+            raise ValueError(
+                "OUTPUT_FILE must contain only letters, numbers, '_'"
+                " and end with .txt"
+            )
+        return self
+
+
+def get_config(filename: str) -> Config:
+    """Retrieve the data from the indicated file.
+
+    Retrieve every data, try to convert them into the correct type and
+    send them in config_storage for further checking.
+
+    Handle errors internally, raise them then terminate the program.
+
+    Return dict with all the data ready to use
+
+    Use:
+        config = get_config("filename.txt")
+    """
+    config = Config()
+    try:
+        with open(filename, "r") as a:
+            for line in a:
+                line = line.strip()
+                if not line:
+                    continue
+                key, value = line.split("=")
+
+                if key in {"WIDTH", "HEIGHT"}:
+                    if value.isdigit():
+                        if key == "HEIGHT":
+                            config.height = int(value)
+                        else:
+                            config.width = int(value)
+                    else:
+                        raise ValueError("Size error, invalid input")
+
+                if key in {"ENTRY", "EXIT"}:
+                    if "," in value:
+                        parts = value.split(",")
+                        if len(parts) != 2:
+                            raise ValueError("Entry or Exit error, invalid"
+                                             " coordinates.\nExemple: 5,8")
+                        if parts[0].isdigit() and parts[1].isdigit():
+                            if key == "ENTRY":
+                                config.entry = [int(parts[0]), int(parts[1])]
+                            else:
+                                config.exit = [int(parts[0]), int(parts[1])]
+                        else:
+                            raise ValueError("Entry or Exit error, invalid"
+                                             " coordinates.\nExemple: 5,8")
+                    else:
+                        raise ValueError("Entry or Exit error, invalid "
+                                         "coordinates.\nExemple: 5,8")
+
+                if key == "OUTPUT_FILE":
+                    if not value.endswith(".txt"):
+                        raise ValueError("OUTPUT_FILE error, invalid name")
+
+                if key == "PERFECT":
+                    if value == "True":
+                        config.perfect = True
+                    elif value == "False":
+                        config.perfect = False
+                    else:
+                        raise ValueError("Type error, what kind of maze do you"
+                                         " want?")
+
+                if key == "SEED":
+                    if value == "True":
+                        config.seed = True
+                    elif value == "False":
+                        config.seed = False
+
+        try:
+            tester = config_storage(width=config.width,
+                                    height=config.height,
+                                    entry=config.entry,
+                                    exit=config.exit,
+                                    output_file=config.output_file,
+                                    perfect=config.perfect)
+        except ValidationError as err:
+            for e in err.errors():
+                print(e["msg"])
+            sys.exit()
+        tester = tester
+    except FileNotFoundError:
+        print("File not found")
+        sys.exit()
+    except PermissionError:
+        print("No permission")
+        sys.exit()
+    except ValueError:
+        print("Invalid data type")
+        sys.exit()
+    return config
+
+
 # execute color_map pour obtenir le maze coloré
 def render(maze: list[list[maze_generator.Cell]], height: int, width: int,
            color_ind: int, show_path: bool) -> None:
@@ -87,16 +215,9 @@ def render(maze: list[list[maze_generator.Cell]], height: int, width: int,
         color_map(maze, height, width, color_ind, show_path)
 
 
-def gen_maze(data: maze_generator.Config) -> list[list[maze_generator.Cell]]:
-
-    maze = maze_generator.maze_creator(data.height, data.width)
-    maze_generator.fourtier(maze, data.height, data.width)
-    maze_generator.atributor_start(maze, data.entry)
-    maze_generator.atributor_exit(maze, data.exit)
-    maze_generator.wanderer(maze, data.entry, data)
-    if not data.perfect:
-        maze_generator.imperfect_maker(maze, data)
-        maze_generator.imperfect_solver(maze, data)
+def gen_maze(mazegen: MazeGenerator) -> list[list[maze_generator.Cell]]:
+    map = mazegen.generate()
+    maze = mazegen.solve(map)
     return maze
 
 
@@ -104,8 +225,9 @@ def gen_maze(data: maze_generator.Config) -> list[list[maze_generator.Cell]]:
 def ui(filename: str) -> None:
     show_path = True
     color_ind = 0
-    data = maze_generator.get_config(filename)
-    mazegen = gen_maze(data)
+    data = get_config(filename)
+    map = MazeGenerator(data)
+    mazegen = gen_maze(map)
     maze_generator.generate_output_file(data.output_file, mazegen, data.entry,
                                         data.exit, data.path)
     render(mazegen, data.height, data.width, color_ind, show_path)
@@ -119,7 +241,7 @@ def ui(filename: str) -> None:
         param = input("Choice? (1-4): ").strip()
         while param != "4":
             if param == "1":
-                mazegen = gen_maze(data)
+                mazegen = gen_maze(map)
                 maze_generator.generate_output_file(data.output_file, mazegen,
                                                     data.entry,
                                                     data.exit, data.path)
@@ -131,7 +253,7 @@ def ui(filename: str) -> None:
                 color_ind = rotate_colors(color_ind)
                 render(mazegen, data.height, data.width, color_ind, show_path)
             else:
-                mazegen = gen_maze(data)
+                mazegen = gen_maze(map)
                 maze_generator.generate_output_file(data.output_file, mazegen,
                                                     data.entry,
                                                     data.exit, data.path)
--- RENDU/maze_generator/maze_generator.py
+++ RENDU/maze_generator/maze_generator.py
@@ -1,7 +1,6 @@
 import random
 import sys
-from pydantic import BaseModel, Field, model_validator, ValidationError
-import re
+from config_parsing import Config
 from typing import List
 from dataclasses import dataclass
 
@@ -221,171 +220,6 @@ class Flameling(Creature):
     def attack(self) -> str:
         return ("Flameling uses Ember!")
 """
-
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-
-class Config():
-    width: int = 0
-    height: int = 0
-    entry: list[int]
-    exit: list[int]
-    output_file: str = "output_maze.txt"
-    perfect: bool = True
-    seed: bool = False
-    path: list[list[int]]  # ****
-
-
-class config_storage(BaseModel):
-    """Multiple check for the config file input."""
-    width: int = Field(ge=2, le=50)
-    height: int = Field(ge=2, le=20)
-    entry: list[int]
-    exit: list[int]
-    output_file: str
-    perfect: bool
-
-    @model_validator(mode='after')
-    def checker(self) -> "config_storage":
-        if len(self.entry) != 2:
-            raise ValueError("Entry error, invalid coordinates")
-        if len(self.exit) != 2:
-            raise ValueError("Exit error, invalid coordinates")
-        if (
-           self.entry[0] < 0 or self.entry[0] >= self.width or
-           self.entry[1] < 0 or self.entry[1] >= self.height):
-            raise ValueError("Entry error, invalid coordinates")
-        if (
-           self.exit[0] < 0 or self.exit[0] >= self.width or
-           self.exit[1] < 0 or self.exit[1] >= self.height):
-            raise ValueError("Exit error, invalid coordinates")
-
-        if not re.fullmatch(r"[A-Za-z0-9_]+\.txt", self.output_file):
-            raise ValueError(
-                "OUTPUT_FILE must contain only letters, numbers, '_'"
-                " and end with .txt"
-            )
-        return self
-
-
-def get_config(filename: str) -> Config:
-    """Retrieve the data from the indicated file.
-
-    Retrieve every data, try to convert them into the correct type and
-    send them in config_storage for further checking.
-
-    Handle errors internally, raise them then terminate the program.
-
-    Return dict with all the data ready to use
-
-    Use:
-        config = get_config("filename.txt")
-    """
-    config = Config()
-    try:
-        with open(filename, "r") as a:
-            for line in a:
-                line = line.strip()
-                if not line:
-                    continue
-                key, value = line.split("=")
-
-                if key in {"WIDTH", "HEIGHT"}:
-                    if value.isdigit():
-                        if key == "HEIGHT":
-                            config.height = int(value)
-                        else:
-                            config.width = int(value)
-                    else:
-                        raise ValueError("Size error, invalid input")
-
-                if key in {"ENTRY", "EXIT"}:
-                    if "," in value:
-                        parts = value.split(",")
-                        if len(parts) != 2:
-                            raise ValueError("Entry or Exit error, invalid"
-                                             " coordinates.\nExemple: 5,8")
-                        if parts[0].isdigit() and parts[1].isdigit():
-                            if key == "ENTRY":
-                                config.entry = [int(parts[0]), int(parts[1])]
-                            else:
-                                config.exit = [int(parts[0]), int(parts[1])]
-                        else:
-                            raise ValueError("Entry or Exit error, invalid"
-                                             " coordinates.\nExemple: 5,8")
-                    else:
-                        raise ValueError("Entry or Exit error, invalid "
-                                         "coordinates.\nExemple: 5,8")
-
-                if key == "OUTPUT_FILE":
-                    if not value.endswith(".txt"):
-                        raise ValueError("OUTPUT_FILE error, invalid name")
-
-                if key == "PERFECT":
-                    if value == "True":
-                        config.perfect = True
-                    elif value == "False":
-                        config.perfect = False
-                    else:
-                        raise ValueError("Type error, what kind of maze do you"
-                                         " want?")
-
-                if key == "SEED":
-                    if value == "True":
-                        config.seed = True
-                    elif value == "False":
-                        config.seed = False
-
-        try:
-            tester = config_storage(width=config.width,
-                                    height=config.height,
-                                    entry=config.entry,
-                                    exit=config.exit,
-                                    output_file=config.output_file,
-                                    perfect=config.perfect)
-        except ValidationError as err:
-            for e in err.errors():
-                print(e["msg"])
-            sys.exit()
-        tester = tester
-    except FileNotFoundError:
-        print("File not found")
-        sys.exit()
-    except PermissionError:
-        print("No permission")
-        sys.exit()
-    except ValueError:
-        print("Invalid data type")
-        sys.exit()
-    return config
-
-
-# def main() -> None:
-#     print("")
-#     a = get_config("config.txt")
-#     print(a)
-#     width = a.width
-#     height = a.height
-#     entry = a.entry
-#     exit = a.exit
-#     output_file = a.output_file
-#     perfect = a.perfect
-#     seed = a.seed
-#     print("")
-#     print(width, height, entry, exit, output_file, perfect, seed)
-
-
-# if __name__ == "__main__":
-#     main()
 
 
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -948,3 +782,21 @@ def way_maker(maze: list[list[Cell]], data: dict[str, bool | str | int | list],
 
 # if __name__ == "__main__":
 #     main()
+
+
+class MazeGenerator:
+    def __init__(self, config: Config):
+        self.config = config
+
+    def generate(self) -> list[list[Cell]]:
+        return maze_creator(self.config.height, self.config.width)
+
+    def solve(self, genmaze: list[list[Cell]]) -> list[list[Cell]]:
+        fourtier(genmaze, self.config.height, self.config.width)
+        atributor_start(genmaze, self.config.entry)
+        atributor_exit(genmaze, self.config.exit)
+        wanderer(genmaze, self.config.entry, self.config)
+        if not self.config.perfect:
+            imperfect_maker(genmaze, self.config)
+            imperfect_solver(genmaze, self.config)
+        return genmaze
